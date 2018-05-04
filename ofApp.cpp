39a567b82835ec7d534b9f6914750a18ffa0fb8f
Xuan Ho <teoorxuan@gmail.com>
2018-05-03 18:26:19 -0700

    change mars to moon-boudinobj, add lighting setup

---
+++ ofApp.cpp
@@ -0,0 +1,645 @@
+
+//--------------------------------------------------------------
+//
+//  Kevin M. Smith
+//
+//  Mars HiRise Project - startup scene
+// 
+//  This is an openFrameworks 3D scene that includes an EasyCam
+//  and example 3D geometry which I have reconstructed from Mars
+//  HiRis photographs taken the Mars Reconnaisance Orbiter
+//
+//  You will use this source file (and include file) as a starting point
+//  to implement assignment 5  (Parts I and II)
+//
+//  Please do not modify any of the keymappings.  I would like 
+//  the input interface to be the same for each student's 
+//  work.  Please also add your name/date below.
+
+//  Please document/comment all of your work !
+//  Have Fun !!
+//
+//  Student Name:   Justin M. Armijo
+//  Date: 19 April 2018
+
+
+#include "ofApp.h"
+#include "Util.h"
+
+
+
+//--------------------------------------------------------------
+// setup scene, lighting, state and load geometry
+//
+void ofApp::setup(){
+
+	bWireframe = false;
+	bDisplayPoints = false;
+	bAltKeyDown = false;
+	bCtrlKeyDown = false;
+	bRoverLoaded = false;
+	bTerrainSelected = true;
+//	ofSetWindowShape(1024, 768);
+	cam.setDistance(10);
+	cam.setNearClip(.1);
+	cam.setFov(65.5);   // approx equivalent to 28mm in 35mm format
+	ofSetVerticalSync(true);
+	cam.disableMouseInput();
+	ofEnableSmoothing();
+	ofEnableDepthTest();
+	ofEnableLighting();
+
+	// Setup 3 - Light System
+	// 
+	keyLight.setup();
+	keyLight.enable();
+	keyLight.setAreaLight(1, 1);
+	keyLight.setAmbientColor(ofFloatColor(0.1, 0.1, 0.1));
+	keyLight.setDiffuseColor(ofFloatColor(1, 1, 1));
+	keyLight.setSpecularColor(ofFloatColor(1, 1, 1));
+
+	keyLight.rotate(45, ofVec3f(0, 1, 0));
+	keyLight.rotate(-45, ofVec3f(1, 0, 0));
+	keyLight.setPosition(5, 5, 5);
+
+	fillLight.setup();
+	fillLight.enable();
+	fillLight.setSpotlight();
+	fillLight.setScale(.05);
+	fillLight.setSpotlightCutOff(15);
+	fillLight.setAttenuation(2, .001, .001);
+	fillLight.setAmbientColor(ofFloatColor(0.1, 0.1, 0.1));
+	fillLight.setDiffuseColor(ofFloatColor(1, 1, 1));
+	fillLight.setSpecularColor(ofFloatColor(0, 34, 0));
+	fillLight.rotate(-10, ofVec3f(1, 0, 0));
+	fillLight.rotate(-45, ofVec3f(0, 1, 0));
+	fillLight.setPosition(-5, 5, 5);
+
+	rimLight.setup();
+	rimLight.enable();
+	rimLight.setSpotlight();
+	rimLight.setScale(.05);
+	rimLight.setSpotlightCutOff(30);
+	rimLight.setAttenuation(.2, .001, .001);
+	rimLight.setAmbientColor(ofFloatColor(0.1, 0.1, 0.1));
+	rimLight.setDiffuseColor(ofFloatColor(1, 1, 1));
+	rimLight.setSpecularColor(ofFloatColor(1, 1, 1));
+	rimLight.rotate(180, ofVec3f(0, 1, 0));
+	rimLight.setPosition(0, 5, -7);
+
+	
+	// setup rudimentary lighting 
+	//
+	initLightingAndMaterials();
+
+	//load moon model  moon-houdini
+	moon.loadModel("geo/moon-houdini.obj");
+	moon.setScaleNormalization(false);
+	lander.loadModel("geo/lander.obj");
+	lander.setScaleNormalization(false);
+
+	boundingBox = meshBounds(moon.getMesh(0));
+
+	//  Test Box Subdivide
+	//
+	//subDivideBox8(boundingBox, level1);
+	//subDivideBox8(level1[0], level2);
+	//subDivideBox8(level2[0], level3);
+	mesh = moon.getMesh(0);
+	for (int i = 0; i < mesh.getNumVertices(); i++) {
+		root.points.push_back(i);
+	}
+	root.Box = boundingBox;
+	buildTree(root, -1);
+	
+
+	responseTime = 0.0;
+}
+
+//--------------------------------------------------------------
+// incrementally update scene (animation)
+//
+void ofApp::update() {
+	
+}
+//--------------------------------------------------------------
+void ofApp::draw(){
+	
+//	ofBackgroundGradient(ofColor(20), ofColor(0));   // pick your own backgroujnd
+	ofBackground(ofColor::black);
+//	cout << ofGetFrameRate() << endl;
+
+	cam.begin();
+	ofPushMatrix();
+	
+	if (bWireframe) {                    // wireframe mode  (include axis)
+		ofDisableLighting();
+		ofSetColor(ofColor::slateGray);
+		moon.drawWireframe();
+		if (bRoverLoaded) {
+			lander.drawWireframe();
+			if (!bTerrainSelected) drawAxis(lander.getPosition());
+		}
+		if (bTerrainSelected) drawAxis(ofVec3f(0, 0, 0));
+	}
+	else {
+		ofEnableLighting();              // shaded mode
+		moon.drawFaces();
+
+		if (bRoverLoaded) {
+			lander.drawFaces();
+			if (!bTerrainSelected) drawAxis(lander.getPosition());
+		}
+		if (bTerrainSelected) drawAxis(ofVec3f(0, 0, 0));
+	}
+
+
+	if (bDisplayPoints) {                // display points as an option    
+		glPointSize(3);
+		ofSetColor(ofColor::green);
+		moon.drawVertices();
+	}
+
+	// highlight selected point (draw sphere around selected point)
+	//
+	if (bPointSelected) {
+		ofSetColor(ofColor::blue);
+		ofDrawSphere(selectedPoint, .1);
+	}
+	
+	ofNoFill();
+	ofSetColor(ofColor::white);
+	drawBox(boundingBox);
+
+	// draw all the lights 
+	//
+	ofSetColor(ofColor::aqua);
+	keyLight.draw();
+	fillLight.draw();
+	rimLight.draw();
+
+	ofSetColor(ofColor::red);
+	for (int i=0; i < level1.size(); i++)
+		drawBox(level1[i]);
+
+	ofSetColor(ofColor::blue);
+	for (int i = 0; i < level2.size(); i++)
+		drawBox(level2[i]);
+
+	ofSetColor(ofColor::yellow);
+	for (int i = 0; i < level3.size(); i++)
+		drawBox(level3[i]);
+	
+	drawOct(root, 5, 0);
+	
+	//This was what I originally used to test the drawing while I was debugging the recursion draw
+	/*for (int i = 0; i < 8; i++) {
+		drawBox(root.children[i].Box);
+		for (int j = 0; j < root.children[i].children.size(); j++) {
+			ofSetColor(ofColor::blue);
+			drawBox(root.children[i].children[j].Box);
+			for (int k = 0; k < root.children[i].children[j].children.size(); k++) {
+				ofSetColor(ofColor::green);
+				drawBox(root.children[i].children[j].children[k].Box);
+			}
+		}
+	}*/
+	
+
+	ofPopMatrix();
+	cam.end();
+}
+
+// 
+
+// Draw an XYZ axis in RGB at world (0,0,0) for reference.
+//
+void ofApp::drawAxis(ofVec3f location) {
+
+	ofPushMatrix();
+	ofTranslate(location);
+
+	ofSetLineWidth(1.0);
+
+	// X Axis
+	ofSetColor(ofColor(255, 0, 0));
+	ofDrawLine(ofPoint(0, 0, 0), ofPoint(1, 0, 0));
+	
+
+	// Y Axis
+	ofSetColor(ofColor(0, 255, 0));
+	ofDrawLine(ofPoint(0, 0, 0), ofPoint(0, 1, 0));
+
+	// Z Axis
+	ofSetColor(ofColor(0, 0, 255));
+	ofDrawLine(ofPoint(0, 0, 0), ofPoint(0, 0, 1));
+
+	ofPopMatrix();
+}
+
+
+void ofApp::keyPressed(int key) {
+
+	switch (key) {
+	case 'C':
+	case 'c':
+		if (cam.getMouseInputEnabled()) cam.disableMouseInput();
+		else cam.enableMouseInput();
+		break;
+	case 'F':
+	case 'f':
+		ofToggleFullscreen();
+		break;
+	case 'H':
+	case 'h':
+		break;
+	case 'r':
+		cam.reset();
+		break;
+	case 's':
+		savePicture();
+		break;
+	case 't':
+		setCameraTarget();
+		break;
+	case 'u':
+		break;
+	case 'v':
+		togglePointsDisplay();
+		break;
+	case 'V':
+		break;
+	case 'w':
+		toggleWireframeMode();
+		break;
+	case OF_KEY_ALT:
+		cam.enableMouseInput();
+		bAltKeyDown = true;
+		break;
+	case OF_KEY_CONTROL:
+		bCtrlKeyDown = true;
+		break;
+	case OF_KEY_SHIFT:
+		break;
+	case OF_KEY_DEL:
+		break;
+	default:
+		break;
+	}
+}
+
+void ofApp::toggleWireframeMode() {
+	bWireframe = !bWireframe;
+}
+
+void ofApp::toggleSelectTerrain() {
+	bTerrainSelected = !bTerrainSelected;
+}
+
+void ofApp::togglePointsDisplay() {
+	bDisplayPoints = !bDisplayPoints;
+}
+
+void ofApp::keyReleased(int key) {
+
+	switch (key) {
+	
+	case OF_KEY_ALT:
+		cam.disableMouseInput();
+		bAltKeyDown = false;
+		break;
+	case OF_KEY_CONTROL:
+		bCtrlKeyDown = false;
+		break;
+	case OF_KEY_SHIFT:
+		break;
+	default:
+		break;
+
+	}
+}
+
+
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+}
+
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+    ofVec3f mouse(mouseX, mouseY);
+	ofVec3f rayPoint = cam.screenToWorld(mouse);
+	ofVec3f rayDir = rayPoint - cam.getPosition();
+	rayDir.normalize();
+	Ray ray = Ray(Vector3(rayPoint.x, rayPoint.y, rayPoint.z),
+		Vector3(rayDir.x, rayDir.y, rayDir.z));
+	float startTime = ofGetElapsedTimeMicros();
+	recSearch(ray, root, selectedPoint);
+	float endTime = ofGetElapsedTimeMicros();
+	responseTime = endTime - startTime;
+	string rt;
+	rt += "Response Time: " + std::to_string(responseTime);
+	cout << rt << endl;
+}
+
+
+//draw a box from a "Box" class  
+//
+void ofApp::drawBox(const Box &box) {
+	Vector3 min = box.parameters[0];
+	Vector3 max = box.parameters[1];
+	Vector3 size = max - min;
+	Vector3 center = size / 2 + min;
+	ofVec3f p = ofVec3f(center.x(), center.y(), center.z());
+	float w = size.x();
+	float h = size.y();
+	float d = size.z();
+	ofDrawBox(p, w, h, d);
+}
+
+// return a Mesh Bounding Box for the entire Mesh
+//
+Box ofApp::meshBounds(const ofMesh & mesh) {
+	int n = mesh.getNumVertices();
+	ofVec3f v = mesh.getVertex(0);
+	ofVec3f max = v;
+	ofVec3f min = v;
+	for (int i = 1; i < n; i++) {
+		ofVec3f v = mesh.getVertex(i);
+
+		if (v.x > max.x) max.x = v.x;
+		else if (v.x < min.x) min.x = v.x;
+
+		if (v.y > max.y) max.y = v.y;
+		else if (v.y < min.y) min.y = v.y;
+
+		if (v.z > max.z) max.z = v.z;
+		else if (v.z < min.z) min.z = v.z;
+	}
+	return Box(Vector3(min.x, min.y, min.z), Vector3(max.x, max.y, max.z));
+}
+
+//  Subdivide a Box into eight(8) equal size boxes, return them in boxList;
+//
+void ofApp::subDivideBox8(const Box &box, vector<Box> & boxList) {
+	Vector3 min = box.parameters[0];
+	Vector3 max = box.parameters[1];
+	Vector3 size = max - min;
+	Vector3 center = size / 2 + min;
+	float xdist = (max.x() - min.x()) / 2;
+	float ydist = (max.y() - min.y()) / 2;
+	float zdist = (max.z() - min.z()) / 2;
+	Vector3 h = Vector3(0, ydist, 0);
+
+	//  generate ground floor
+	//
+	Box b[8];
+	b[0] = Box(min, center);
+	b[1] = Box(b[0].min() + Vector3(xdist, 0, 0), b[0].max() + Vector3(xdist, 0, 0));
+	b[2] = Box(b[1].min() + Vector3(0, 0, zdist), b[1].max() + Vector3(0, 0, zdist));
+	b[3] = Box(b[2].min() + Vector3(-xdist, 0, 0), b[2].max() + Vector3(-xdist, 0, 0));
+
+	boxList.clear();
+	for (int i = 0; i < 4; i++)
+		boxList.push_back(b[i]);
+
+	// generate second story
+	//
+	for (int i = 4; i < 8; i++) {
+		b[i] = Box(b[i - 4].min() + h, b[i - 4].max() + h);
+		boxList.push_back(b[i]);
+	}
+}
+
+void ofApp::buildTree(TreeNode & root, int interator)
+{
+	vector<Box> divisions;
+	subDivideBox8(root.Box, divisions);
+	vector<TreeNode> tempLeaves;
+	for (int k = 0; k < 8; k++) {
+		TreeNode temp;
+		temp.Box = divisions[k];
+		tempLeaves.push_back(temp);
+	}
+	for (int i = 0; i < root.points.size(); i++) {
+		for (int j = 0; j < 8; j++) {
+			ofVec3f p = mesh.getVertex(root.points[i]);
+			Vector3 v = Vector3(p.x, p.y, p.z);
+			if (tempLeaves[j].Box.inside(v)) {
+				tempLeaves[j].points.push_back(root.points[i]);
+			}
+		}
+	}
+	for (int i = 0; i < 8; i++) {
+		if (tempLeaves[i].points.size() > 1) {			
+			root.children.push_back(tempLeaves[i]);
+			//buildTree(root.children.back(), (interator));
+
+			//if (interator > 0 || interator <= -1) {
+				//interator--;
+			buildTree(root.children.back(), (interator));
+				
+			//}
+		}
+		if (tempLeaves[i].points.size() == 1) {
+			root.children.push_back(tempLeaves[i]);
+			
+		}
+	}
+		
+}
+void ofApp::drawOct(TreeNode & node, int numLevels, int level) {
+	if (level >= numLevels) return;
+	drawBox(node.Box);
+	
+	level++;
+	for (int i = 0; i < node.children.size(); i++) {
+		drawOct(node.children[i], numLevels, level);
+		
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+
+//
+//  Select Target Point on Terrain by comparing distance of mouse to 
+//  vertice points projected onto screenspace.
+//  if a point is selected, return true, else return false;
+//
+bool ofApp::doPointSelection() {
+
+	ofMesh mesh = moon.getMesh(0);
+	int n = mesh.getNumVertices();
+	float nearestDistance = 0;
+	int nearestIndex = 0;
+
+	bPointSelected = false;
+
+	ofVec2f mouse(mouseX, mouseY);
+	vector<ofVec3f> selection;
+
+	// We check through the mesh vertices to see which ones
+	// are "close" to the mouse point in screen space.  If we find 
+	// points that are close, we store them in a vector (dynamic array)
+	//
+	for (int i = 0; i < n; i++) {
+		ofVec3f vert = mesh.getVertex(i);
+		ofVec3f posScreen = cam.worldToScreen(vert);
+		float distance = posScreen.distance(mouse);
+		if (distance < selectionRange) {
+			selection.push_back(vert);
+			bPointSelected = true;
+		}
+	}
+
+	//  if we found selected points, we need to determine which
+	//  one is closest to the eye (camera). That one is our selected target.
+	//
+	if (bPointSelected) {
+		float distance = 0;
+		for (int i = 0; i < selection.size(); i++) {
+			ofVec3f point =  cam.worldToCamera(selection[i]);
+
+			// In camera space, the camera is at (0,0,0), so distance from 
+			// the camera is simply the length of the point vector
+			//
+			float curDist = point.length(); 
+
+			if (i == 0 || curDist < distance) {
+				distance = curDist;
+				selectedPoint = selection[i];
+			}
+		}
+	}
+	return bPointSelected;
+}
+
+// Set the camera to use the selected point as it's new target
+//  
+void ofApp::setCameraTarget() {
+
+}
+
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+
+
+//--------------------------------------------------------------
+// setup basic ambient lighting in GL  (for now, enable just 1 light)
+//
+void ofApp::initLightingAndMaterials() {
+
+	static float ambient[] =
+	{ .5f, .5f, .5, 1.0f };
+	static float diffuse[] =
+	{ 1.0f, 1.0f, 1.0f, 1.0f };
+
+	static float position[] =
+	{5.0, 5.0, 5.0, 0.0 };
+
+	static float lmodel_ambient[] =
+	{ 1.0f, 1.0f, 1.0f, 1.0f };
+
+	static float lmodel_twoside[] =
+	{ GL_TRUE };
+
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+
+	glLightfv(GL_LIGHT1, GL_AMBIENT, ambient);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuse);
+	glLightfv(GL_LIGHT1, GL_POSITION, position);
+
+
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+	glLightModelfv(GL_LIGHT_MODEL_TWO_SIDE, lmodel_twoside);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+//	glEnable(GL_LIGHT1);
+	glShadeModel(GL_SMOOTH);
+} 
+
+void ofApp::savePicture() {
+	ofImage picture;
+	picture.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+	picture.save("screenshot.png");
+	cout << "picture saved" << endl;
+}
+
+//--------------------------------------------------------------
+//
+// support drag-and-drop of model (.obj) file loading.  when
+// model is dropped in viewport, place origin under cursor
+//
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+	ofVec3f point;
+	mouseIntersectPlane(ofVec3f(0, 0, 0), cam.getZAxis(), point);
+
+	if (lander.loadModel(dragInfo.files[0])) {
+		lander.setScaleNormalization(false);
+		lander.setScale(.005, .005, .005);
+		lander.setPosition(point.x, point.y, point.z);
+		bRoverLoaded = true;
+	}
+	else cout << "Error: Can't load model" << dragInfo.files[0] << endl;
+}
+
+bool ofApp::mouseIntersectPlane(ofVec3f planePoint, ofVec3f planeNorm, ofVec3f &point) {
+	ofVec2f mouse(mouseX, mouseY);
+	ofVec3f rayPoint = cam.screenToWorld(mouse);
+	ofVec3f rayDir = rayPoint - cam.getPosition();
+	rayDir.normalize();
+	return (rayIntersectPlane(rayPoint, rayDir, planePoint, planeNorm, point));
+}
+
+void ofApp::recSearch(Ray ray, TreeNode &root, ofVec3f &selected) {
+	
+	for (int i = 0; i < root.children.size(); i++) {
+		
+		if (root.children[i].Box.intersect(ray, -100, 100)) {
+			if (root.children[i].children.size() == 0) {
+				int index = root.children[i].points[0];
+				selected = mesh.getVertex(index);
+				bPointSelected = true;
+			}
+			else {
+				recSearch(ray, root.children[i], selected);
+			}
+		}
+	}
+
+}
---
+++ ofApp.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "ofMain.h"
+#include  "ofxAssimpModelLoader.h"
+#include "box.h"
+#include "ray.h"
+
+
+
+class TreeNode {
+	public:
+		Box Box;
+		vector<int> points;
+		vector<TreeNode> children;
+		
+
+};
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		void drawAxis(ofVec3f);
+		void initLightingAndMaterials();
+		void savePicture();
+		void toggleWireframeMode();
+		void togglePointsDisplay();
+		void toggleSelectTerrain();
+		void setCameraTarget();
+		bool  doPointSelection();
+		void drawBox(const Box &box);
+		Box meshBounds(const ofMesh &);
+		void subDivideBox8(const Box &b, vector<Box> & boxList);
+		void buildTree(TreeNode & root, int interator);
+		bool mouseIntersectPlane(ofVec3f planePoint, ofVec3f planeNorm, ofVec3f &point);
+		void drawOct(TreeNode & node, int numLevels, int level);
+		void recSearch(Ray ray, TreeNode &node, ofVec3f &selected);
+		ofEasyCam cam;
+		ofLight light;
+		Box boundingBox;
+		vector<Box> level1, level2, level3;
+	
+		bool bAltKeyDown;
+		bool bCtrlKeyDown;
+		bool bWireframe;
+		bool bDisplayPoints;
+		bool bPointSelected;
+		
+		//Octree part
+		bool bRoverLoaded;
+		bool bTerrainSelected;
+		float responseTime;
+		ofVec3f selectedPoint;
+		ofVec3f intersectPoint;
+		ofMesh mesh;
+		TreeNode root;
+		const float selectionRange = 4.0;
+
+		//lighting & lander part
+		ofxAssimpModelLoader moon, lander; //model = lander
+		ofLight keyLight, fillLight, rimLight;
+		ofPlanePrimitive plane;
+		ofMaterial planeMaterial;
+
+		bool bModelLoaded = false;
+		bool bPlaneLoaded = false;
+		bool bWireFrame = false;
+};
